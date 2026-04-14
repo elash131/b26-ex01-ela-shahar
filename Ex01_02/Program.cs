21e0338ea1606079eb09ec7f0167146f14099f92
Nusssh <nusssh@mta.ac.il>
2026-04-14 18:59:13 +0300

    Add Ex01_02 letter tree project

---
+++ Ex01_02/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Ex01_02
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.Write(LetterTreeBuilder.BuildTree((int)eTreeMeasurements.FixedTreeHeight));
+		}
+	}
+}
